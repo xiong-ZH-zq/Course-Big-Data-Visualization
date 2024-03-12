1c8b2266f20c54c6adfb6df16122bc4a9670bcf3
xzqbear <909117948@qq.com>
2024-03-12 10:54:36 +0800

    Week 3 Wordcloud

--- Week 3/词频词云图.py
+++ Week 3/词频词云图.py
@@ -1,16 +1,21 @@
 import pandas as pd  
 from wordcloud import WordCloud  
 import matplotlib.pyplot as plt  
-  
+import numpy as np
+from PIL import Image
+
 # 读取词频文件  
 df = pd.read_csv('Week 3\sitetotal_分词后_词频.tsv',delimiter='\t')  
 
 text = ' '.join(df['word'])
 
+mask = np.array(Image.open("Week 3/mask.png"))
+
 # 创建词云对象  
 wordcloud = WordCloud(
-    width=800, 
-    height=400, 
+    width=500, 
+    mask=mask,
+    height=500, 
     background_color='white', 
     min_font_size=10,
     font_path=r"Week 3\SarasaFixedSC-Regular.ttf").generate(text)  
@@ -18,5 +23,6 @@ wordcloud = WordCloud(
 # 显示词云图  
 plt.figure(figsize=(10, 5))  
 plt.imshow(wordcloud, interpolation='bilinear')  
-plt.axis('off')  
-plt.show()
+plt.axis('off')
+plt.savefig("Week 3/Wordcloud.png",dpi=960)  
+# plt.show()
